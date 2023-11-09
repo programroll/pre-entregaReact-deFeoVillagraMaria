bba268c60eb08f5b7d4665ffa907c2385d105da8
programroll <programroll@gmail.com>
2023-11-09 11:57:48 -0300

    contador arreglado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer.js';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemCount from './components/ItemCount/ItemCount.jsx';
 
@@ -10,8 +10,10 @@ function App() {
     <div className="App">
       <NavBar/>
       <ItemListContainer greeting={'Bienvenidos'}/>
+      <ItemCount />
+      
     </div>
-
+    
   );
 }
 
--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,4 +1,22 @@
 import './styles-ItemCount.css'
+import {useState} from "react";
+
+const ItemCount = () =>{
+
+const [count,setCount]=useState(0)
+const handleSumar=()=>{setCount (count+1);};
+const handleRestar = () => {setCount(count - 1);};
+
+return( <div>
+    <button onClick={handleRestar}>-</button>
+    <span>{count}</span>
+    <button onClick={handleSumar}>+</button>
+</div>
+);
+
+};
+
+
 
 
 export default ItemCount
